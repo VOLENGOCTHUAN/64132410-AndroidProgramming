313daccc9314524e891536d25e7bf4a0a3b688a0
VOLENGOCTHUAN <thuan.vln.64cntt@ntu.edu.vn>
2025-04-27 01:59:54 +0700

    up

--- Bottom_Navigation_ViewFragment/app/src/main/java/tiil/edu/bottom_navigation_view_fragment/Fragment_ListView.java
+++ Bottom_Navigation_ViewFragment/app/src/main/java/tiil/edu/bottom_navigation_view_fragment/Fragment_ListView.java
@@ -1,5 +1,6 @@
 package tiil.edu.bottom_navigation_view_fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,36 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_ListView#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment_ListView extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    ListView listview;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public Fragment_ListView() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_ListView.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_ListView newInstance(String param1, String param2) {
         Fragment_ListView fragment = new Fragment_ListView();
         Bundle args = new Bundle();
@@ -55,10 +40,19 @@ public class Fragment_ListView extends Fragment {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__list_view, container, false);
+        View view_listview = inflater.inflate(R.layout.fragment__list_view, container, false);
+        listview = view_listview.findViewById(R.id.listview);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+        });
+
+        return view_listview;
     }
 }
